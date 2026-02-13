26304ea33b9c5595c0611e94469865d40588610c
joshlian <jtl5655@mavs.uta.edu>
2026-02-12 22:40:48 -0600

    changed file name

--- event-service/src/main/java/com/venuex/event_service/repository/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.venuex.event_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.venuex.event_service.entities.Venue;
-
-public interface VenueRepository extends JpaRepository<Venue, Integer> {
-    boolean existsByName(String name);
-}
