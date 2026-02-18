a0242f20ba40c23c843795a9d06f473c2187d829
immafirinmalazer <kang.ashlee397@gmail.com>
2026-02-18 03:28:25 -0600

    fixed bad import for EnableEurekaServer

--- eureka/src/main/java/com/venuex/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/venuex/eureka/EurekaApplication.java
@@ -2,7 +2,7 @@ package com.venuex.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
